e0623ef7f11769a42c60bfb6f97ceca6e2ad38e5
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-09 14:59:49 +0200

    incapsulated the id

--- demo/src/main/java/com/example/demo/Dev.java
+++ demo/src/main/java/com/example/demo/Dev.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
 public class Dev {
+  private int age;
+  
 
+    public Dev () {
+        System.out.println("Constructor of the dev");
+    }
 public  void build (){
     System.out.println("working on owersome project");
+}
+public int getAge() {
+    return age;
+}
+public void setAge(int age) {
+    this.age = age;
 }
 
 }
